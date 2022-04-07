e4f57ab8370f1b26211716bd792b9d7c33c39a38
bisrat <bisrat.amtataw@gmail.com>
2022-04-07 16:15:46 -0400

    phase2

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// document.querySelector('.greenBtn').
+// Defining variables 
+let player = [];
+let simon = [];
+const arrayColors = ['green','red','yellow','blue'];
+
+const green = document.querySelector('.greenBtn')
+// console.log(green)
+const red = document.querySelector('.redBtn')
+// console.log(red)
+const start = document.querySelector('.startBtn')
+// console.log(start)
+const yellow = document.querySelector('.yellowBtn')
+// console.log(yellow)
+const blue = document.querySelector('.blueBtn')
+// console.log(blue)
+const reset = document.querySelector('.resetBtn')
+// console.log(reset)
