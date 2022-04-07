a3ca4db9053d56de793295461713fd2cf8acb754
bisrat <bisrat.amtataw@gmail.com>
2022-04-07 16:36:18 -0400

    phase2_lateafternoon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Defining variables 
 let player = [];
-let simon = [];
+let simon = ['green','red','yellow','blue'];
 const arrayColors = ['green','red','yellow','blue'];
 
 const green = document.querySelector('.greenBtn')
@@ -15,3 +15,25 @@ const blue = document.querySelector('.blueBtn')
 // console.log(blue)
 const reset = document.querySelector('.resetBtn')
 // console.log(reset)
+
+// adding eventListener for each button
+start.addEventListener('click', ()=> {
+    // console.log("start clicked")
+})
+
+green.addEventListener('click', ()=> {
+    // console.log("green clicked")
+})
+red.addEventListener('click', ()=> {
+    // console.log("red clicked")
+})
+blue.addEventListener('click', ()=> {
+    // console.log("blue clicked")
+})
+yellow.addEventListener('click', ()=> {
+    // console.log("yellow clicked")
+})
+
+reset.addEventListener('click', ()=> {
+    // console.log("reset clicked")
+})
